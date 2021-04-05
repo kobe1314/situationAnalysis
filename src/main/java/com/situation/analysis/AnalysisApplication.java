b0a58dec10ae656b38452c8507331f2270744161
kobe <kobegor@aliyun.com>
2021-04-05 19:58:47 +0800

    push

--- src/main/java/com/situation/analysis/AnalysisApplication.java
+++ src/main/java/com/situation/analysis/AnalysisApplication.java
@@ -4,6 +4,13 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.mybatis.spring.annotation.MapperScans;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.server.ConfigurableWebServerFactory;
+import org.springframework.boot.web.server.ErrorPage;
+import org.springframework.boot.web.server.WebServerFactoryCustomizer;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpStatus;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 /**
@@ -12,10 +19,18 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @EnableTransactionManagement
 @MapperScan("com.situation.analysis.mapper")
 @SpringBootApplication
-public class AnalysisApplication {
+public class AnalysisApplication{
 
     public static void main(String[] args) {
         SpringApplication.run(AnalysisApplication.class, args);
+    }
+
+    @Bean
+    public WebServerFactoryCustomizer<ConfigurableWebServerFactory> webServerFactoryCustomizer(){
+        return factory -> {
+            ErrorPage error404Page = new ErrorPage(HttpStatus.NOT_FOUND, "/index.html");
+            factory.addErrorPages(error404Page);
+        };
     }
 
 }
