c5e681da8eef26e3623682593cd8b39d62bda837
yaoSir <yaosir88@163.com>
2021-10-27 23:20:08 +0800

    /ticket support base64 file and files

--- src/main/java/com/situation/analysis/service/BusinessService.java
+++ src/main/java/com/situation/analysis/service/BusinessService.java
@@ -3,6 +3,7 @@ package com.situation.analysis.service;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
+import java.util.List;
 
 /**
  * @description: business service
@@ -11,5 +12,5 @@ import java.io.IOException;
  * @version: v1.0
  */
 public interface BusinessService {
-    Object deliveryTicket(MultipartFile file, String orderId, String orderNum, String empId) throws IOException;
+    Object deliveryTicket(List<MultipartFile> files, String orderId, String orderNum, String empId) throws IOException;
 }
